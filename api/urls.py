70e0be6588d5c94e986bf3258419e0e12f6c91ef
Giacomo Zanotti <giacomozanotti.dev@gmail.com>
2025-11-20 09:29:51 +0100

    wip adding income expense #1

--- api/urls.py
+++ api/urls.py
@@ -23,7 +23,8 @@ from api.views.csv_upload_view import CsvUploadView, CsvUploadDelete, CsvProcess
     CsvUploadCheckView
 from api.views.rule_view import RuleDefineView, RuleDeleteView
 from api.views.monthly_summary_view import MonthlySummerView
-from api.views.transaction_view import TransactionListView, EditTransactionCategory, TransactionDetailUpdateView
+from api.views.transaction_view import TransactionListView, EditTransactionCategory, TransactionDetailUpdateView, \
+    TransactionIncomeCreateView
 
 urlpatterns = [
     path("accounts/", login_form, name="login_form"),
@@ -38,6 +39,7 @@ urlpatterns = [
     path('transactions/upload/<int:pk>/process', CsvProcessView.as_view(), name='transactions_process_detail'),
     path('transactions/upload/<int:pk>/delete/', CsvUploadDelete.as_view(), name='transactions_upload_delete'),
     path('transactions/', TransactionListView.as_view(), name='transaction_list'),
+    path('transactions/income/new', TransactionIncomeCreateView.as_view(), name='new_transaction_income'),
     path('transactions/<int:pk>/', TransactionDetailUpdateView.as_view(), name='transaction_detail'),
     path('transactions/rules/<int:pk>/delete/', RuleDeleteView.as_view(), name='delete_rule'),
     path('transactions/rules/', RuleDefineView.as_view(), name='define_rule'),
--- api/views/transaction_view.py
+++ api/views/transaction_view.py
@@ -10,11 +10,118 @@ from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse
 from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from django.views.generic import UpdateView
 
 from api.models import Transaction, Category, Rule, Merchant
 
+
+class TransactionIncomeCreateView(LoginRequiredMixin, CreateView):
+    """
+    A view to create a new Transaction instance of type income.
+    Simplified form with only date and amount required.
+    """
+    model = Transaction
+    template_name = 'transactions/transaction_income.html'
+    fields = [
+        'transaction_date',
+        'amount',
+    ]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        from datetime import date
+        context['today'] = date.today()
+        return context
+
+    @transaction.atomic
+    def form_valid(self, form):
+        """
+        Set proper transaction defaults for income type.
+        Income transactions don't require merchant or category.
+        """
+        # Set transaction defaults for income
+        form.instance.user = self.request.user
+        form.instance.transaction_type = 'income'
+        form.instance.description = self.request.POST.get('description', 'Entrata Manuale')
+        form.instance.modified_by_user = True
+        form.instance.status = 'categorized'
+
+        # Income transactions typically don't have a merchant
+        form.instance.merchant = None
+        form.instance.merchant_raw_name = 'Entrata'
+
+        self.object = form.save()
+
+        # Redirect to transaction list after successful creation
+        return redirect('transaction_list')
+
+    def get_success_url(self):
+        """Redirect to transaction list after successful creation"""
+        return reverse('transaction_list')
+
+class TransactionCreateView(LoginRequiredMixin, CreateView):
+    """
+    A view to create a new Transaction instance of type expense.
+    Handles all transaction data including merchant, category, and amount.
+    """
+    model = Transaction
+    template_name = 'transactions/transaction_expense_create.html'
+    fields = [
+        'transaction_date',
+        'amount',
+        'merchant_raw_name',
+        'description',
+        'category'
+    ]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.filter(
+            Q(user=self.request.user) | Q(is_default=True)
+        ).distinct()
+        return context
+
+    @transaction.atomic
+    def form_valid(self, form):
+        """
+        Customizes form validation to handle new category creation,
+        merchant assignment, and set proper transaction defaults.
+        """
+        # Handle new category creation
+        new_category_name = self.request.POST.get('new_category_name', '').strip()
+
+        if new_category_name:
+            new_category, created = Category.objects.get_or_create(
+                name=new_category_name,
+                user=self.request.user,
+                defaults={'is_default': False}
+            )
+            form.instance.category = new_category
+
+        # Handle merchant
+        merchant_name = self.request.POST.get('merchant_raw_name', '').strip()
+
+        if merchant_name:
+            merchant_db = Merchant.get_similar_merchants_by_names(merchant_name, self.request.user).first()
+            if not merchant_db:
+                merchant_db = Merchant.objects.create(name=merchant_name, user=self.request.user)
+            form.instance.merchant = merchant_db
+
+        # Set transaction defaults
+        form.instance.user = self.request.user
+        form.instance.transaction_type = 'expense'
+        form.instance.modified_by_user = True
+        form.instance.status = 'categorized'
+
+        self.object = form.save()
+
+        # Redirect to transaction list after successful creation
+        return redirect('transaction_list')
+
+    def get_success_url(self):
+        """Redirect to transaction list after successful creation"""
+        return reverse('transaction_list')
 
 class TransactionDetailUpdateView(LoginRequiredMixin, UpdateView):
     """
