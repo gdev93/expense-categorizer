db012f93c164b51c4863bb496a668275a3b747fc
Giacomo Zanotti <giacomozanotti.dev@gmail.com>
2026-02-09 11:51:35 +0100

    caching on disk the hugging face model to avoid download issues

--- processors/embeddings.py
+++ processors/embeddings.py
@@ -1,6 +1,5 @@
 import os
 import threading
-
 from fastembed import TextEmbedding
 
 
@@ -12,8 +11,12 @@ class EmbeddingEngine:
     def get_model(cls):
         with cls._lock:
             if cls._instance is None:
+                # Define the cache directory from environment or default to a local folder
+                cache_dir = os.environ.get('FASTEMBED_CACHE_PATH', './model_cache')
+
                 cls._instance = TextEmbedding(
-                        model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2",
-                        threads=int(os.environ.get('EMBEDDED_THREADS_NUMBER', 4))
-                    )
+                    model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2",
+                    threads=int(os.environ.get('EMBEDDED_THREADS_NUMBER', 4)),
+                    cache_dir=cache_dir  # This ensures it looks into the persistent volume
+                )
         return cls._instance
