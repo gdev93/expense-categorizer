83df81a9802c9cd7f7f2f905294b2c1340831444
Giacomo Zanotti <giacomozanotti.dev@gmail.com>
2026-02-14 11:21:04 +0100

    google oauth configuration completed

--- api/apps.py
+++ api/apps.py
@@ -6,3 +6,6 @@ from django.db.models.signals import post_save
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
+
+    def ready(self):
+        import api.signals
